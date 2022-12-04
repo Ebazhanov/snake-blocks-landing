dad79c50b0deaef30e9dc8d6fbd65466cb41dd96
Evgenii <evg.bazhanov@yahoo.com>
2022-12-04 20:20:45 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+      <div className="main-block">
+        <h1>How it works</h1>
+        <div className="working-blocks">
+          <div className="working-block">
+            Applications open
+          </div>
+          <div className="working-block">
+            Complete & pass <br/> a screening test
+          </div>
+          <div className="working-block">
+            Tutorial starts
+          </div>
+          <div className="working-block">
+            Receive exam results <br/> and certificate
+          </div>
+          <div className="working-block">
+            Complete your <br/> exam
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
